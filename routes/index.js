040eb9c1e4e57d76d290d4a3ca1ca7abf2a95699
William Stevens <wastevensv@gmail.com>
2015-02-26 17:11:30 -0500

    Added product list page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var fortune = require('fortune');
 var router = express.Router();
+var db = fortune({db: 'scoutnet'}).adapter;
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'FISH' });
+  db.findMany('product').then( function(list) {
+      res.render('productlist', { title: 'FISH', products:list });
+});
 });
 
 module.exports = router;
