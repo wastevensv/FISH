1c65716e16cebbbebe6bbce9ce336696ee1f14a7
William Stevens <wastevensv@gmail.com>
2015-03-04 18:27:51 -0500

    List of lists

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,15 @@ var db = fortune({db: 'scoutnet'}).adapter;
 
 function findById(source, id) {
   for (var i = 0; i < source.length; i++) {
+    if(source[i].id == id) {
+      return source[i];
+    }
+  }
+  return null;
+}
+
+function findByUPC(source, id) {
+  for (var i = 0; i < source.length; i++) {
     if(source[i].upc === id) {
       return source[i];
     }
@@ -13,22 +22,43 @@ function findById(source, id) {
   return null;
 }
 
-
 /* GET home page. */
 router.get('/', function(req, res) {
-  db.findMany('product').then( function(list) {
-    for(var i = 0; i < list.length; i++) {
-      list[i].info = findById(upcdata.data, list[i].barcode);
-    }
-    list.sort(function (a, b) {
-      now = Date.now();
-      dta = now - a.expires;
-      dtb = now - b.expires;
-      return (dta < dtb) ? 1 : ((dta > dtb) ? -1 : 0);
-    })
+  db.findMany('list').then( function(lists) {
+    db.findMany('product').then( function(products) {
+      for(var i = 0; i < products.length; i++) {
+        products[i].info = findByUPC(upcdata.data, products[i].barcode);
+      }
+      products.sort(function (a, b) {
+        now = Date.now();
+        dta = now - a.expires;
+        dtb = now - b.expires;
+        return (dta < dtb) ? 1 : ((dta > dtb) ? -1 : 0);
+      })
 
-    res.render('productlist', { title: 'Inventory', products:list });
+      res.render('productlist', { title: 'Inventory', products:products, lists:lists });
+    });
   });
 });
 
+router.get('/list/:id', function(req, res) {
+  db.findMany('list').then( function(lists) {
+    list = findById(lists, req.params.id);
+    console.log(list.links.items)
+    db.findMany('product', list.links.items).then( function(products) {
+      console.log(products)
+      for(var i = 0; i < products.length; i++) {
+        products[i].info = findByUPC(upcdata.data, products[i].barcode);
+      }
+      products.sort(function (a, b) {
+        now = Date.now();
+        dta = now - a.expires;
+        dtb = now - b.expires;
+        return (dta < dtb) ? 1 : ((dta > dtb) ? -1 : 0);
+      })
+  
+      res.render('productlist', { title: list.name, products:products, lists:lists });
+    });
+  });
+});
 module.exports = router;
